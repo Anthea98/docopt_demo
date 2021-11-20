688442ae5ff79f45ac6ac066a442031f1b98e0ad
Anthea98 <44433495+Anthea98@users.noreply.github.com>
2021-11-19 22:02:27 -0800

    Add an optional positional argument

--- src/demo.R
+++ src/demo.R
@@ -1,10 +1,11 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Anthea Chen
+# date: 2021-11-19
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> [<arg_op>] --arg2=<arg2> [--arg3=<arg3>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
+[<arg_op>]          Takes any value (this is a optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
@@ -13,3 +14,4 @@ library(docopt)
 opt <- docopt(doc)
 print(opt)
 print(typeof(opt))
+print(opt$arg_op)
